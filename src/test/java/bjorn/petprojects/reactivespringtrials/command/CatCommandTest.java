c15e566893b95bf8a11efad5488eed4b36f7dc66
Björn De Craemer <bjorn.decraemer@yahoo.com>
2018-11-27 02:06:22 +0100

    Added some more Tests

--- src/test/java/bjorn/petprojects/reactivespringtrials/command/CatCommandTest.java
+++ src/test/java/bjorn/petprojects/reactivespringtrials/command/CatCommandTest.java
@@ -4,7 +4,11 @@ import bjorn.petprojects.reactivespringtrials.model.Cat;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
@@ -15,7 +19,12 @@ public class CatCommandTest {
     Cat jack = new Cat("Jack", "Exotic-Panda");
     Cat roger = new Cat("Roger", "Exotic-Panda");
     Cat whiskey = new Cat("Whiskey", "Exotic-Lapjeskat");
+    Flux<Cat> cats;
 
+    @Before
+    public void initialize(){
+         cats = Flux.just(chihiro,jack,roger,whiskey);
+    }
 
     @Test
     public void monoTest() {
@@ -48,6 +57,40 @@ public class CatCommandTest {
         log.info(rogerAxe.describeMe());
     }
 
-    
+    @Test
+    public void fluxTest() throws Exception{
+
+        cats.subscribe(cat -> log.info(cat.describeMe()));
+    }
+
+    @Test
+    public void fluxTestFilter() throws Exception{
+
+        cats.filter(cat -> cat.getName().equalsIgnoreCase("Chihiro") || cat.getName().equalsIgnoreCase("Jack"))
+                .subscribe(cat -> log.info(cat.describeMe()));
+    }
+
+    @Test
+    public void fluxTestDelay() throws Exception{
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        cats.delayElements(Duration.ofSeconds(1))
+                .doOnComplete(countDownLatch::countDown)
+                .subscribe(cat -> log.info(cat.describeMe()));
+        countDownLatch.await();
+    }
+
+    @Test
+    public void fluxTestFilterDelay() throws Exception{
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        cats.delayElements(Duration.ofSeconds(1))
+                .filter(cat -> cat.getName().contains("i"))
+                .doOnComplete(countDownLatch::countDown)
+                .subscribe(cat -> log.info(cat.describeMe()));
+        countDownLatch.await();
+    }
 
 }
